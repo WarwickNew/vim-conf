9e104a03fc2dcddb3b2df8a290b3238e37b32b96
Warwick <warwick.l.e.new@gmail.com>
2023-08-09 16:07:39 +0100

    Changed bind as I want to be able to make git commits in the term

--- lua/core/plugin_config/terminal.lua
+++ lua/core/plugin_config/terminal.lua
@@ -7,7 +7,7 @@ require("toggleterm").setup({
 -- Get out of terminal!
 function _G.set_terminal_keymaps()
   local opts = {buffer = 0}
-  vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
+  vim.keymap.set('t', '<leader><esc>', [[<C-\><C-n>]], opts)
   vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
 end
 vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
