de6db6ac9794d52cab9a719f8fc7a5cb580ab32a
Warwick <warwick.l.e.new@gmail.com>
2023-08-17 15:13:28 +0100

    added window management to leader key

--- lua/core/plugin_config/whichkey.lua
+++ lua/core/plugin_config/whichkey.lua
@@ -32,8 +32,29 @@ local keymap = {
     g = { '<Cmd>Telescope git_commits<CR>', 'Commits' },
     b = { '<Cmd>Telescope git_branches<CR>', 'Branches' },
     s = { '<Cmd>Telescope git_status<CR>', 'Status' },
+  },
+  w = {
+    name = '+window',
+    w = 'Pick Window',
+    s = "Split window",
+    v = "Split window vertically",
+    q = "Quit a window",
+    T = "Break out into a new tab",
+    x = "Swap current with next",
+    ["-"] = "Decrease height",
+    ["+"] = "Increase height",
+    ["<lt>"] = "Decrease width",
+    [">"] = "Increase width",
+    ["|"] = "Max out the width",
+    ["="] = "Equally high and wide",
+    h = "Go to the left window",
+    l = "Go to the right window",
+    k = "Go to the up window",
+    j = "Go to the down window",
   }
 }
+-- Map window keys to leader w
+vim.cmd[[nmap <leader>w <C-w>]]
 
 whichkey.setup {
   hide_statusline = false,
