d609082e0992e4a47e8628897bdcd0762ae48e18
Warwick New <warwick.l.e.new@gmail.com>
2024-08-20 10:55:58 +0100

    Fixed template issue with daily note.

--- lua/core/plugin_config/obsidian.lua
+++ lua/core/plugin_config/obsidian.lua
@@ -9,16 +9,16 @@ require("obsidian").setup({
     -- Set to false to disable completion.
     nvim_cmp = true,
   },
-  daily_notes = {
-    -- Optional, if you keep daily notes in a separate directory.
-    folder = "diary",
-    -- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
-    template = "Templates/daily_note"
-  },
   templates = {
     subdir = "Templates",
     -- A map for custom variables, the key should be the variable and the value a function
     substitutions = {},
+  },
+  daily_notes = {
+    -- Optional, if you keep daily notes in a separate directory.
+    folder = "diary",
+    -- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
+    template = "daily_note.md"
   },
   note_id_func = function(title)
     local suffix = ""
